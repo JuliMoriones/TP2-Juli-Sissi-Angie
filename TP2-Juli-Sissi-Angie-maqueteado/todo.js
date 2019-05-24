c08b1350d3015bae32aa089543f7a83cdd5cfc30
steph.michelfelder <stephanie@moorea.io>
2019-05-23 21:26:44 -0300

    trabajo terminado

---
+++ TP2-Juli-Sissi-Angie-maqueteado/todo.js
@@ -0,0 +1,87 @@
+var allTasks = []
+var completed, allButtons, item, listItem, newTask
+
+
+var keyPress = function (event) {
+    if (event.keyCode === 13) {
+        addTask()
+    }
+}
+
+var blankText = function (list, noText) {
+    if (list.getElementsByTagName("li").length < 1) {
+        var text = document.createElement("li")
+        text.classList.add("blank")
+        text.innerText = noText
+        list.appendChild(text)
+    }
+}
+
+var createLi = function (task, index) {
+    // crea las li
+    listItem = document.createElement('li')
+    listItem.innerText = task.text
+    listItem.classList.add('task')
+    var toggleImage;
+
+    allButtons = document.createElement('div')
+    allButtons.classList.add('task-btns')
+    //condicional para cambiar icono pending por completed cuando clickeas
+    if(!task.isCompleted){
+        toggleImage = 'pending'
+    } else {        //gracias mike por tu magia
+        toggleImage = 'completed'
+        listItem.classList.add('line')
+    }
+
+
+    allButtons.appendChild(createButton('trash', index, deleteTask))
+    allButtons.appendChild(createButton(toggleImage, index, toggleTask))
+
+    listItem.appendChild(allButtons)
+    return listItem
+}
+
+var createButton = function (classBtn, index, btnFunction) {
+    btn = document.createElement('button')
+    btn.innerHTML = '<img src="styles/images/' + classBtn + '.svg">'
+    btn.id = index
+    btn.onclick = function () { btnFunction(this) }
+    return btn
+}
+
+
+var toggleTask = function (btn) {
+    allTasks[btn.id].isCompleted = !allTasks[btn.id].isCompleted
+
+    printTask()
+}
+
+var deleteTask = function (btn) {
+    allTasks.splice(btn.id, 1)
+    printTask()
+}
+
+var printTask = function () {
+    toDo = document.getElementById('todo')
+    toDo.innerHTML = ''
+    completed = document.getElementById('completed')
+    completed.innerHTML = ''
+
+    allTasks.map(function (task, index) {
+        task.isCompleted ? completed.appendChild(createLi(task, index)) : todo.appendChild(createLi(task, index))
+    })
+    blankText(toDo, '¡Excelente!' + '\n\nNo tenés ninguna tarea pendiente')
+    blankText(completed, '¡Ánimo!' + '\n\nCuando completes tus tareas van a aparecer acá')
+}
+
+var addTask = function () {
+    item = document.getElementById('item')
+    newTask = item.value
+
+    if (newTask !== '') {
+        item.value = ''
+        allTasks.unshift({ text: newTask, isCompleted: false })
+        printTask()
+    }
+}
