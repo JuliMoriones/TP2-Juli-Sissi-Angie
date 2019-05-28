b86ed9ff02ab13266032adb5fd698339b1f1a820
amichelfelder <47372761+amichelfelder@users.noreply.github.com>
2019-05-28 19:24:01 -0300

    Edit button (ANGIE SOS LO MAS)

--- todo.js
+++ todo.js
@@ -1,7 +1,6 @@
 var allTasks = []
 var completed, allButtons, item, listItem, newTask
 
-
 var keyPress = function (event) {
     if (event.keyCode === 13) {
         addTask()
@@ -20,38 +19,34 @@ var blankText = function (list, noText) {
 
 var createLi = function (task, index) {
     listItem = document.createElement('li')
+	listItem.id=index;
     listItem.innerText = task.text
     listItem.classList.add('task')
-
-    var input = document.createElement('input');
-    input.value = task.text
-    input.disabled = true
-    input.classList.add('hidden')
-	
     var toggleImage;
 
     allButtons = document.createElement('div')
     allButtons.classList.add('task-btns')
     if(!task.isCompleted){
         toggleImage = 'pending'
-    } else {  
+    } else {        //gracias mike por tu magia
         toggleImage = 'completed'
         listItem.classList.add('line')
     }
 
     allButtons.appendChild(createButton('trash', index, deleteTask))
     allButtons.appendChild(createButton(toggleImage, index, toggleTask))
-    allButtons.appendChild(createButton('edit', index, editTask))
+	allButtons.appendChild(createButton('edit', index, editTask))
     listItem.appendChild(allButtons)
+
+    
     return listItem
 
 }
 
-
 var createButton = function (classBtn, index, btnFunction) {
     btn = document.createElement('button')
-    btn.innerHTML = '<img src="styles/images/' + classBtn + '.svg">'
-    btn.id = index
+    btn.innerHTML = '<img src="styles/images/' + classBtn + '.svg" alt="' + classBtn +'">'
+    btn.id = index;
     btn.onclick = function () { btnFunction(this) }
     return btn
 }
@@ -60,7 +55,6 @@ var toggleTask = function (btn) {
     allTasks[btn.id].isCompleted = !allTasks[btn.id].isCompleted
     printTask()
 }
-
 
 var deleteTask = function (btn) {
     var deleteTask = confirm('¿Estás segurx de borrar esta tarea?');
@@ -73,12 +67,6 @@ var deleteTask = function (btn) {
 
 }
 
-var editTask = function (btn) {
-   
-     input.disabled = false;
-     input.classList.remove('hidden')
-     input.onKeyPress = function(event){ actuallyEditItem(e, input) }
-}
 var printTask = function () {
     toDo = document.getElementById('todo')
     toDo.innerHTML = ''
@@ -90,7 +78,7 @@ var printTask = function () {
     })
     blankText(toDo, '¡Excelente!' + '\n\nNo tenés ninguna tarea pendiente')
     blankText(completed, '¡Ánimo!' + '\n\nCuando completes tus tareas van a aparecer acá')
-    guardarTarea();
+	guardarTarea();
 }
 
 var addTask = function () {
@@ -102,7 +90,6 @@ var addTask = function () {
         allTasks.unshift({ text: newTask, isCompleted: false })
         printTask()
     }
-
 }
 
 var guardarTarea = function(){
@@ -116,7 +103,6 @@ var prinTarea = function(){
     if (allTasks == null){
         allTasks = [];
     }
-    
 }
 
 var onLoad = function(){
@@ -124,19 +110,29 @@ var onLoad = function(){
     printTask()
 }
 
-//var editItem = function(){
-    // input.disabled = false;
-    // input.classList.remove('hidden')
-    // input.onKeyPress = function(event){ actuallyEditItem(e, input) }
-//}
+var editTask = function (btn) {
+	var posicion = btn.id;
+	var task = allTasks[ posicion ];
 
-//var actuallyEditItem = function(e, input){ allTasks[btn.id].text = input.value}
+	var input = document.createElement('input');
+    input.value = task.text
+	input.id = posicion;
+	input.onkeydown = function (event) {
+		if (event.keyCode === 13) { 
+			actuallyEditItem(event, this);
+		}
+	}
 	
-	
-//	ar createLi = function (task, index) {
-    // crea las li
-    //listItem = document.createElement('li')
-    //var input = cretae input;
-    //input.value = task.text
-    //input.disabled = true (fijate)
-    //input.classlist.add('hidden')
+	var listItem = btn.parentNode.parentNode;
+	listItem.appendChild(input)
+}
+
+var actuallyEditItem = function( event, input ) {
+	var texto = input.value;
+	var posicion = input.id;
+	var task = allTasks[ posicion ];
+	task.text = texto;
+	allTasks[ posicion ] = task;
+	input.parentNode.removeChild(input);
+	printTask();
+}
